2bec11a9d930bd18debc250b2000d5558f591157
charu sharma <charu7636@gmail.com>
2025-09-13 07:29:54 +0530

    added

---
+++ array/sorted.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class sorted{
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the size of the array here : ");
+
+        int n= sc.nextInt();
+        int []a=new int[n];
+        for(int i=0;i<n;i++){
+            System.out.print("enter the elements : ");
+            a[i]=sc.nextInt();
+        }
+        Boolean ans=is_sorted(a,n);
+            if(ans==true){
+                System.out.print("yes");
+            }else{
+                System.out.print("No");
+            }
+        
+       
+    }
+     public static boolean is_sorted(int[]a,int n){
+        for(int i=1;i<n;i++){
+            if (a[i]<a[i-1]){
+                return false;
+            }
+           
+        }
+     return true;}}
