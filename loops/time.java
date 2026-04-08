1edd67b74fc90498070dfea1b75b5b7fd5e187ac
charu sharma <charu7636@gmail.com>
2026-04-09 03:52:19 +0530

    add

---
+++ loops/time.java
@@ -0,0 +1,11 @@
+package loops;
+
+public class time {
+    public static void main(String[] args) {
+        int n;
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        n= sc.nextInt();
+        for (int i=1;i<=n;i++){
+            System.out.println(i+" second(s)");}
+    sc.close();}
+}
