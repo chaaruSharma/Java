594dd52a464bf9d0d92f4299e6982bf440d03f5b
charu sharma <charu7636@gmail.com>
2026-04-09 03:52:14 +0530

    add

---
+++ loops/once.java
@@ -0,0 +1,14 @@
+package loops;
+
+
+public class once {
+    public static void main(String[] args) {
+        int n;
+        java.util.Scanner sc = new java.util.Scanner (System.in);
+        n= sc.nextInt();
+        for (int i=1;i<=n;i++){
+            System.out.println("Hello World");}
+    sc.close();
+
+    }
+}
