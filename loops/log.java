c7545b7d14a89646eabfe7f6aa5845be735e0af0
charu sharma <charu7636@gmail.com>
2026-04-09 03:51:53 +0530

    add

---
+++ loops/log.java
@@ -0,0 +1,12 @@
+package loops;
+
+public class log {
+    public static void main(String[] args) {
+        int n;
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        n= sc.nextInt();
+        for (int i=1;i<=n;i++){
+            System.out.println((int)(Math.log(i)));}
+    sc.close();}
+    
+}
